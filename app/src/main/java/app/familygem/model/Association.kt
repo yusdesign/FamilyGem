b1fe9612cca262ec7efbc55a8663ea79845ca7b7
yus <swiftcase@gmail.com>
2026-08-09 13:01:52 +0800

    Update Association.kt

--- app/src/main/java/app/familygem/model/Association.kt
+++ app/src/main/java/app/familygem/model/Association.kt
@@ -1,10 +1,8 @@
 package app.familygem.model
 
-import java.io.Serializable
-
 class Association(
     var personId: String? = null,
     var relationship: String? = null,
     var type: String? = null,
     var details: String? = null
-) : Serializable
+)
