f382e597544d2ec9e09d95ca517c8b97bde2876d
yus <swiftcase@gmail.com>
2026-08-09 13:01:29 +0800

    Update PersonWrapper.kt

--- app/src/main/java/app/familygem/model/PersonWrapper.kt
+++ app/src/main/java/app/familygem/model/PersonWrapper.kt
@@ -1,10 +1,8 @@
 package app.familygem.model
 
-import java.io.Serializable
-
 class PersonWrapper(
     val personId: String
-) : Serializable {
+) {
     var socialEstate: String? = null
     var associations: MutableList<Association> = mutableListOf()
 
