927b1566575d0fa65e96ff828ee678c23ef4db6b
yus <swiftcase@gmail.com>
2026-08-09 19:41:33 +0800

    Update ProfileActivity.kt

--- app/src/main/java/app/familygem/profile/ProfileActivity.kt
+++ app/src/main/java/app/familygem/profile/ProfileActivity.kt
@@ -3,6 +3,7 @@ package app.familygem.profile
 import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
